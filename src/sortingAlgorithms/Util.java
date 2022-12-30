3f22b56d9d3bfed77146ff90c554ef0a3af8c856
Bugaienko <sergiibugaienko@gmail.com>
2022-12-30 16:39:36 +0100

    Update Util.java

--- src/sortingAlgorithms/Util.java
+++ src/sortingAlgorithms/Util.java
@@ -2,7 +2,7 @@ package sortingAlgorithms;
 
 import java.util.List;
 
-public  class Util {
+public class Util {
     public static Element[] initArray(int length) {
         Element[] result = new Element[length];
         for (int i = 0; i < length; i++) {
@@ -13,16 +13,17 @@ public  class Util {
 
     public static void printArray(Element[] arr) {
         StringBuilder sb = new StringBuilder("[ ");
-        for (Element element: arr){
+        for (Element element : arr) {
             sb.append(element.toString());
             sb.append("; ");
         }
         sb.append("]");
         System.out.println(sb.toString());
     }
+
     public static void printArray(List<Element> arr) {
         StringBuilder sb = new StringBuilder("[ ");
-        for (Element element: arr){
+        for (Element element : arr) {
             sb.append(element.toString());
             sb.append("; ");
         }
